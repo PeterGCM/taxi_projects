091c70bc1ba397574bbf53413ed3fa2b4a539335
jerryhan88 <jerryhan88@gmail.com>
2016-05-20 08:11:50 +0900

    t

--- analysis_information_board_around_airport/b_aggregated_analysis/b_generate_summary/b_s2_merged_trips.py
+++ analysis_information_board_around_airport/b_aggregated_analysis/b_generate_summary/b_s2_merged_trips.py
@@ -3,8 +3,8 @@ from __future__ import division
 import os, sys  
 sys.path.append(os.getcwd() + '/../..')
 #
-from supports._setting import airport_trips_dir, ap_trip_prefix
-from supports._setting import nightsafari_trips_dir, ns_trip_prefix
+from supports._setting import ap_trips_economic_profits_dir, ap_trips_ecoprof_prefix
+from supports._setting import ns_trips_economic_profits_dir, ns_trips_ecoprof_prefix
 from supports._setting import summary_dir, Y09_ap_trips, Y10_ap_trips, Y09_ns_trips, Y10_ns_trips
 from supports.etc_functions import check_dir_create
 #
@@ -24,7 +24,7 @@ def process_files(yymm):
     print 'handle the file; %s' % yymm
     #
     ap_target_file = Y09_ap_trips if yymm.startswith('09') else Y10_ap_trips
-    with open('%s/%s%s.csv' % (airport_trips_dir, ap_trip_prefix, yymm), 'rb') as r_csvfile:
+    with open('%s/%s%s.csv' % (ap_trips_economic_profits_dir, ap_trips_ecoprof_prefix, yymm), 'rb') as r_csvfile:
         reader = csv.reader(r_csvfile)
         headers = reader.next()
         if not os.path.exists(ap_target_file):
@@ -37,7 +37,7 @@ def process_files(yymm):
                 writer.writerow(row)
     #
     ns_target_file = Y09_ns_trips if yymm.startswith('09') else Y10_ns_trips
-    with open('%s/%s%s.csv' % (nightsafari_trips_dir, ns_trip_prefix, yymm), 'rb') as r_csvfile:
+    with open('%s/%s%s.csv' % (ns_trips_economic_profits_dir, ns_trips_ecoprof_prefix, yymm), 'rb') as r_csvfile:
         reader = csv.reader(r_csvfile)
         headers = reader.next()
         if not os.path.exists(ns_target_file):
