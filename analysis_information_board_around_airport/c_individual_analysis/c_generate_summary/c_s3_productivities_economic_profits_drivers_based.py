c6ff06b0b8b96a9a3cdc12b9cb1f48f91b00fedd
jerryhan88 <jerryhan88@gmail.com>
2016-05-30 15:57:36 +0800

    t

--- analysis_information_board_around_airport/c_individual_analysis/c_generate_summary/c_s3_productivities_economic_profits_drivers_based.py
+++ analysis_information_board_around_airport/c_individual_analysis/c_generate_summary/c_s3_productivities_economic_profits_drivers_based.py
@@ -22,6 +22,7 @@ _package = [Y09_ftd_general_stat, Y10_ftd_general_stat,
             Y09_ftd_prev_in_ns_stat, Y10_ftd_prev_in_ns_stat,
             Y09_ftd_prev_out_ap_stat, Y10_ftd_prev_out_ap_stat,
             Y09_ftd_prev_out_ns_stat, Y10_ftd_prev_out_ns_stat]
+dfs = [pd.read_csv(path_fn) for path_fn in _package]
 #
 Y09_GEN, Y10_GEN, \
 Y09_PIAP, Y10_PIAP, \
@@ -34,13 +35,11 @@ def run():
     for path in [ftd_general_prod_db, ftd_ap_prod_eco_prof_db, ftd_ns_prod_eco_prof_db]:
         remove_file(path)
     #
-    dfs = [pd.read_csv(path_fn) for path_fn in _package]
-    #
-    save_pickle_file(ftd_general_prod_db , general_productivities(dfs))
-    save_pickle_file(ftd_ap_prod_eco_prof_db, ap_productivities_economical_profits(dfs))
-    save_pickle_file(ftd_ns_prod_eco_prof_db, ns_productivities_economical_profits(dfs))
+    save_pickle_file(ftd_general_prod_db , general_productivities())
+    save_pickle_file(ftd_ap_prod_eco_prof_db, ap_productivities_economical_profits())
+    save_pickle_file(ftd_ns_prod_eco_prof_db, ns_productivities_economical_profits())
 
-def general_productivities(dfs):
+def general_productivities():
     drivers_hourly_producities = []
     for i in [Y09_GEN, Y10_GEN]:
         df_gb = dfs[i].groupby(['did'])
@@ -49,34 +48,17 @@ def general_productivities(dfs):
                   {did : total_prod * SEC3600 / CENT for did, total_prod in drivers_avg_productivities.values})
     return drivers_hourly_producities 
 
-def ap_productivities_economical_profits(dfs):
+def ap_productivities_economical_profits():
     #
     # drivers who operate taxi in both years
     #
     ap_full_drivers = set(dfs[Y09_PIAP]['did'])
     for i in [Y10_PIAP, Y09_POAP, Y10_POAP]:
-        print i
-        print dfs[i]
         ap_full_drivers = ap_full_drivers.intersection(set(dfs[i]['did']))
     #
-    # filter our part-time drivers
-    #   and save each drivers' productivities and economical profits
-    #
-    drivers_ap_prod_eco_prof = []
-    for i in [Y09_PIAP, Y10_PIAP, Y09_POAP, Y10_POAP]:
-        df = dfs[i] 
-        df = df[df['did'].isin(ap_full_drivers)]
-        gb_df = df.groupby(['did'])
-        drivers_ap_prod = gb_df.mean()['ap-prod'].to_frame('avg_ap_prod').reset_index()
-        drivers_ap_prod_hour = {did : ap_prod * SEC3600 / CENT for did, ap_prod in drivers_ap_prod.values}
-        #
-        drivers_eco_prof = gb_df.mean()['ap-eco-profit'].to_frame('avg_eco_pro').reset_index()
-        drivers_eco_prof_month = {did : eco_pro / CENT for did, eco_pro in drivers_eco_prof.values}
-        #
-        drivers_ap_prod_eco_prof.append([drivers_ap_prod_hour, drivers_eco_prof_month])
-    return drivers_ap_prod_eco_prof 
+    return get_driver_average(ap_full_drivers, [Y09_PIAP, Y10_PIAP, Y09_POAP, Y10_POAP])
     
-def ns_productivities_economical_profits(dfs):
+def ns_productivities_economical_profits():
     #
     # drivers who operate taxi in both years
     #
@@ -84,22 +66,26 @@ def ns_productivities_economical_profits(dfs):
     for i in [Y10_PINS, Y09_PONS, Y10_PONS]:
         ns_full_drivers = ns_full_drivers.intersection(set(dfs[i]['did']))
     #
+    return get_driver_average(ns_full_drivers, [Y09_PINS, Y10_PINS, Y09_PONS, Y10_PONS])
+
+def get_driver_average(full_drivers, df_indices):
+    #
     # filter our part-time drivers
     #   and save each drivers' productivities and economical profits
     #
-    drivers_ns_prod_eco_prof = []
-    for i in [Y09_PINS, Y10_PINS, Y09_PONS, Y10_PONS]:
+    drivers_ap_prod_eco_prof = []
+    for i in df_indices:
         df = dfs[i] 
-        df = df[df['did'].isin(ns_full_drivers)]
+        df = df[df['did'].isin(full_drivers)]
         gb_df = df.groupby(['did'])
-        drivers_ns_prod = gb_df.mean()['ns-prod'].to_frame('avg_ns_prod').reset_index()
-        drivers_ns_prod_hour = {did : ns_prod * SEC3600 / CENT for did, ns_prod in drivers_ns_prod.values}
+        drivers_ap_prod = gb_df.mean()['prod'].to_frame('avg_prod').reset_index()
+        drivers_ap_prod_hour = {did : ap_prod * SEC3600 / CENT for did, ap_prod in drivers_ap_prod.values}
         #
-        drivers_eco_prof = gb_df.mean()['ns-eco-profit'].to_frame('avg_eco_pro').reset_index()
+        drivers_eco_prof = gb_df.mean()['eco-profit'].to_frame('avg_eco_pro').reset_index()
         drivers_eco_prof_month = {did : eco_pro / CENT for did, eco_pro in drivers_eco_prof.values}
         #
-        drivers_ns_prod_eco_prof.append([drivers_ns_prod_hour, drivers_eco_prof_month])
-    return drivers_ns_prod_eco_prof 
+        drivers_ap_prod_eco_prof.append([drivers_ap_prod_hour, drivers_eco_prof_month])
+    return drivers_ap_prod_eco_prof
 
 if __name__ == '__main__':
     run()
