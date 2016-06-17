9f0578a961c1aac456fd921c59b9b85b2ba470a4
jerryhan88 <jerryhan88@gmail.com>
2016-06-17 13:05:50 +0800

    t

---
+++ analysis_region_driver_route/init_path_setting.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import os, sys, platform
+#
+taxi_home, taxi_common, taxi_data = None, None, None
+#
+def run():
+    global taxi_home, taxi_common, taxi_data
+    #
+    # Check environments and set a prefix for finding files and libraries
+    #
+    py_vinfo = sys.version_info
+    if py_vinfo.major == 2 and py_vinfo.minor == 7:
+        plf = platform.platform()
+        if plf.startswith('Linux'):
+            # Linux server
+            from subprocess import call
+            call('export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"')
+            call('echo nice')
+            sys.path.append('/home/ckhan/local/lib/python2.7/site-packages')
+            sys.path.append('/home/ckhan/local/lib64/python2.7/site-packages')
+            taxi_data = '/home/ckhan/taxi_data'
+        elif plf.startswith('Darwin'):
+            # Mac
+            taxi_data = '/Users/JerryHan88/taxi_data'
+            pass
+        else:
+            # Window ?
+            taxi_data = 'C:\Users/ckhan.2015/taxi_data'
+            pass
+        sys.path.append(os.getcwd() + '/../taxi_common')
+    else:
+        print 'This python is not 2.7 version'
+        assert False
+
--- analysis_region_driver_route/main.py
+++ analysis_region_driver_route/main.py
@@ -1,5 +1,8 @@
 from __future__ import division
+import init_path_setting; init_path_setting.run() 
 #
+import sys_path_setting
+assert False
 from _setting import zone_visual_info_fn
 from split_into_zones import run as run_split_into_zones
 # from visualizer import run as run_visualizer
