260524d1510c49a1d68eb9d8af3dc9f95c426ac2
jerryhan88 <jerryhan88@gmail.com>
2016-06-22 21:51:13 +0800

    t

--- impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
+++ impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
@@ -6,4 +6,5 @@ from a_overall_analysis import __init__
 #
 IN, OUT = True, False
 #
-ap_poly_fn, ns_poly_fn = '../src/airport_polygon', '../src/night_safari_polygon'
+ap_poly_fn = os.path.dirname(os.path.realpath(__file__)) + '../../src/airport_polygon'
+ns_poly_fn = os.path.dirname(os.path.realpath(__file__)) + '../../src/night_safari_polygon'
