fa47dad62aebb2a49bd69cbfd53243e3cb423029
jerryhan88 <jerryhan88@gmail.com>
2016-06-22 21:37:15 +0800

    t

--- impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
+++ impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import division
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
-for p in sys.path:
-    print p
 #
 from a_overall_analysis import __init__
 #
--- impact_information_boards/init_project.py
+++ impact_information_boards/init_project.py
@@ -1,9 +1,6 @@
 from __future__ import division
 import os, sys
-#
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
-#
-#
 #
 from taxi_common.init_project import get_taxi_home_path, get_taxi_data_path  # @UnresolvedImport
 taxi_home, taxi_data = get_taxi_home_path(), get_taxi_data_path()
