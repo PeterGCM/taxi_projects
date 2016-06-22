ec59d5d7314549a5c04b578c0249f2fe086273cb
jerryhan88 <jerryhan88@gmail.com>
2016-06-22 21:33:02 +0800

    t

--- impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
+++ impact_information_boards/a_overall_analysis/a1_intermediate/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import division
 import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
+for p in sys.path:
+    print p
 #
 from a_overall_analysis import __init__
 #
