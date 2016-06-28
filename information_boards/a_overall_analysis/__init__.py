4c4a3f3b488b1c28468ee32d6ea0c6a57a750d75
jerryhan88 <jerryhan88@gmail.com>
2016-06-28 18:53:27 +0800

    t

--- information_boards/a_overall_analysis/__init__.py
+++ information_boards/a_overall_analysis/__init__.py
@@ -5,11 +5,11 @@ import information_boards.__init__
 #
 from taxi_common.file_handling_functions import check_dir_create
 #
-from __init__ import taxi_data
+from information_boards.__init__ import taxi_data
 check_dir_create(taxi_data)
 trips_dir, trip_prefix = taxi_data + '/trips', 'trip-'
 #
-from __init__ import summary_dir
+from information_boards.__init__ import summary_dir
 check_dir_create(summary_dir)
 ap_tm_num_dur_fare_fn = summary_dir + '/ap-tm-num-dur-fare.csv' 
 ns_tm_num_dur_fare_fn = summary_dir + '/ns-tm-num-dur-fare.csv'
