a5bab9ae7e3da6ae380e1dc6a19fb05aab0035e0
jerryhan88 <jerryhan88@gmail.com>
2016-07-03 17:28:50 +0800

    t

--- information_boards/b_aggregated_analysis/b1_intermediate/b1_i6_hourly_dur_fare_qt.py
+++ information_boards/b_aggregated_analysis/b1_intermediate/b1_i6_hourly_dur_fare_qt.py
@@ -17,17 +17,18 @@ import csv, time, datetime
 def run():
     remove_creat_dir(productivity_dir)
     #
-    init_multiprocessor()
-    count_num_jobs = 0
-    for y in xrange(9, 11):
-        for m in xrange(1, 13):
-            yymm = '%02d%02d' % (y, m)
-            if yymm in ['0912', '1010']:
-                continue
-            # process_files('1007')
-            put_task(process_files, [yymm])
-            count_num_jobs += 1
-    end_multiprocessor(count_num_jobs)
+    process_files('0901')
+    # init_multiprocessor()
+    # count_num_jobs = 0
+    # for y in xrange(9, 11):
+    #     for m in xrange(1, 13):
+    #         yymm = '%02d%02d' % (y, m)
+    #         if yymm in ['0912', '1010']:
+    #             continue
+    #         # process_files('1007')
+    #         put_task(process_files, [yymm])
+    #         count_num_jobs += 1
+    # end_multiprocessor(count_num_jobs)
 
 
 def process_files(yymm):
