9db10ccc5d77996fb6061a31f9eac49402acda1e
JerryHan88 <jerryhan88@gmail.com>
2016-07-09 20:48:25 +0800

    t

--- information_boards/c_individual_analysis/c1_intermediate/c1_i1_full_time_drivers_shift_trip.py
+++ information_boards/c_individual_analysis/c1_intermediate/c1_i1_full_time_drivers_shift_trip.py
@@ -20,10 +20,13 @@ for l, ts in load_pickle_file(zero_duration_timeslots):
         yyyy, mm, dd, hh = 2000 + eval(ts[0]), eval(ts[1]), eval(ts[2]), eval(ts[3])
         omitted_timeslots.append((yyyy, mm, dd, hh))
 
+from traceback import format_exc
+
 
 def run():
     # for path in [ftd_trips_dir, ftd_shift_dir, ftd_list_dir]:
     #     remove_create_dir(path)
+
     init_multiprocessor()
     count_num_jobs = 0
     for yymm in ['1006','1007','1008','1009','1011','1012']:
@@ -43,51 +46,56 @@ def run():
 #     end_multiprocessor(count_num_jobs)
 
 def process_files(yymm):
-    print 'handle the file; %s' % yymm
-    is_driver_vehicle = load_pickle_file('%s/%s%s.pkl' % (vehicle_sharing_dir, vehicle_sharing_prefix, yymm))
-    full_time_drivers = set()
-    with open('%s/%s%s.csv' % (shift_pro_dur_dir, shift_pro_dur_prefix, yymm), 'rt') as r_csvfile:
-        reader = csv.reader(r_csvfile)
-        headers = reader.next()
-        hid = {h : i for i, h in enumerate(headers)}
-        with open('%s/%s%s.csv' % (ftd_shift_dir, ftd_shift_prefix, yymm), 'wt') as w_csvfile:
-            writer = csv.writer(w_csvfile)
-            writer.writerow(headers)
-            for row in reader:
-                if len(is_driver_vehicle[row[hid['vid']]]) > 1:
-                    continue
-                writer.writerow(row)
-                full_time_drivers.add(int(row[hid['did']]))
-    #
-    with open('%s/%s%s.csv' % (trips_dir, trip_prefix, yymm), 'rb') as r_csvfile:
-        reader = csv.reader(r_csvfile)
-        headers = reader.next()
-        hid = {h : i for i, h in enumerate(headers)}
-        with open('%s/%s%s.csv' % (ftd_trips_dir, ftd_trips_prefix, yymm), 'wt') as w_csvfile:
-            writer = csv.writer(w_csvfile)
-            new_headers = ['did', 'start-time', 'duration', 'fare']
-            writer.writerow(new_headers)
-            #
-            # filter out trips data based on two factors;
-            #   1. full time driver
-            #   2. time slots when log data corrupted
-            # 
-            for row in reader:
-                st_ts = eval(row[hid['start-time']])
-                st_dt = datetime.datetime.fromtimestamp(st_ts)
-                k = (st_dt.year, st_dt.month, st_dt.day, st_dt.hour)
-                if k in omitted_timeslots:
-                    continue
-                did = int(row[hid['did']])
-                if did not in full_time_drivers:
-                    continue
-                writer.writerow([row[hid['did']],
-                                 row[hid['start-time']],
-                                 row[hid['duration']],
-                                 row[hid['fare']]])
-    #
-    save_pickle_file('%s/%s%s.pkl' % (ftd_list_dir, ftd_list_prefix, yymm), list(full_time_drivers))
-    print 'end the file; %s' % yymm
+    try:
+        print 'handle the file; %s' % yymm
+        is_driver_vehicle = load_pickle_file('%s/%s%s.pkl' % (vehicle_sharing_dir, vehicle_sharing_prefix, yymm))
+        full_time_drivers = set()
+        with open('%s/%s%s.csv' % (shift_pro_dur_dir, shift_pro_dur_prefix, yymm), 'rt') as r_csvfile:
+            reader = csv.reader(r_csvfile)
+            headers = reader.next()
+            hid = {h : i for i, h in enumerate(headers)}
+            with open('%s/%s%s.csv' % (ftd_shift_dir, ftd_shift_prefix, yymm), 'wt') as w_csvfile:
+                writer = csv.writer(w_csvfile)
+                writer.writerow(headers)
+                for row in reader:
+                    if len(is_driver_vehicle[row[hid['vid']]]) > 1:
+                        continue
+                    writer.writerow(row)
+                    full_time_drivers.add(int(row[hid['did']]))
+        #
+        with open('%s/%s%s.csv' % (trips_dir, trip_prefix, yymm), 'rb') as r_csvfile:
+            reader = csv.reader(r_csvfile)
+            headers = reader.next()
+            hid = {h : i for i, h in enumerate(headers)}
+            with open('%s/%s%s.csv' % (ftd_trips_dir, ftd_trips_prefix, yymm), 'wt') as w_csvfile:
+                writer = csv.writer(w_csvfile)
+                new_headers = ['did', 'start-time', 'duration', 'fare']
+                writer.writerow(new_headers)
+                #
+                # filter out trips data based on two factors;
+                #   1. full time driver
+                #   2. time slots when log data corrupted
+                #
+                for row in reader:
+                    st_ts = eval(row[hid['start-time']])
+                    st_dt = datetime.datetime.fromtimestamp(st_ts)
+                    k = (st_dt.year, st_dt.month, st_dt.day, st_dt.hour)
+                    if k in omitted_timeslots:
+                        continue
+                    did = int(row[hid['did']])
+                    if did not in full_time_drivers:
+                        continue
+                    writer.writerow([row[hid['did']],
+                                     row[hid['start-time']],
+                                     row[hid['duration']],
+                                     row[hid['fare']]])
+        #
+        save_pickle_file('%s/%s%s.pkl' % (ftd_list_dir, ftd_list_prefix, yymm), list(full_time_drivers))
+        print 'end the file; %s' % yymm
+    except Exception as _:
+        with open('logging_Python.txt', 'w') as f:
+            f.write(format_exc())
+        raise
 
 if __name__ == '__main__':
     run()
