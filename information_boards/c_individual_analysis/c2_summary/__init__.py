50da7254dfb84910c8e6fb53c812dfe3dd56580a
JerryHan88 <jerryhan88@gmail.com>
2016-07-10 19:45:46 +0800

    t

--- information_boards/c_individual_analysis/c2_summary/__init__.py
+++ information_boards/c_individual_analysis/c2_summary/__init__.py
@@ -12,15 +12,15 @@ from c_individual_analysis.__init__ import Y09_ftd_prev_in_ns_stat, Y10_ftd_prev
 from c_individual_analysis.__init__ import Y09_ftd_prev_out_ap_stat, Y10_ftd_prev_out_ap_stat
 from c_individual_analysis.__init__ import Y09_ftd_prev_out_ns_stat, Y10_ftd_prev_out_ns_stat
 
-_package = [Y09_ftd_gen_stat, Y10_ftd_gen_stat,
-            Y09_ftd_prev_in_ap_stat, Y10_ftd_prev_in_ap_stat,
-            Y09_ftd_prev_out_ap_stat, Y10_ftd_prev_out_ap_stat,
-            Y09_ftd_prev_in_ns_stat, Y10_ftd_prev_in_ns_stat,
-            Y09_ftd_prev_out_ns_stat, Y10_ftd_prev_out_ns_stat]
-dfs = [pd.read_csv(path_fn) for path_fn in _package]
-#
-Y09_GEN, Y10_GEN, \
-Y09_PIAP, Y10_PIAP, \
-Y09_POAP, Y10_POAP, \
-Y09_PINS, Y10_PINS, \
-Y09_PONS, Y10_PONS = range(10)
+# _package = [Y09_ftd_gen_stat, Y10_ftd_gen_stat,
+#             Y09_ftd_prev_in_ap_stat, Y10_ftd_prev_in_ap_stat,
+#             Y09_ftd_prev_out_ap_stat, Y10_ftd_prev_out_ap_stat,
+#             Y09_ftd_prev_in_ns_stat, Y10_ftd_prev_in_ns_stat,
+#             Y09_ftd_prev_out_ns_stat, Y10_ftd_prev_out_ns_stat]
+# dfs = [pd.read_csv(path_fn) for path_fn in _package]
+# #
+# Y09_GEN, Y10_GEN, \
+# Y09_PIAP, Y10_PIAP, \
+# Y09_POAP, Y10_POAP, \
+# Y09_PINS, Y10_PINS, \
+# Y09_PONS, Y10_PONS = range(10)
