c1e5088e6fdb9af088e04a8a8983858aae4e8a2f
JerryHan88 <jerryhan88@gmail.com>
2016-07-12 15:14:01 +0800

    t

--- community_detection/old_codes/__init__.py
+++ community_detection/old_codes/__init__.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
 #
 from taxi_common.__init__ import get_taxi_home_path
 taxi_home = get_taxi_home_path()
