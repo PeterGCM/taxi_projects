e834d303007af06f785fe825837b26b5709f1413
jerryhan88 <jerryhan88@gmail.com>
2016-07-25 13:57:38 +0800

    t

--- community_detection/d_graph.py
+++ community_detection/d_graph.py
@@ -12,7 +12,7 @@ def run():
     try:
         # process_files('0901')
         # process_within_month('0901')
-        process_files_counting_day('0902')
+        process_files_counting_day('0903')
     except Exception as _:
         with open('logging_Python.txt', 'w') as f:
             f.write(format_exc())
