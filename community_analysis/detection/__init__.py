f66b2b68b8f71d19e50e45d03616448d27a50b76
jerryhan88 <jerryhan88@gmail.com>
2016-08-03 16:06:34 +0800

    t

---
+++ community_analysis/detection/__init__.py
@@ -0,0 +1,10 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/..')
+import community_analysis.__init__
+#
+FREE, POB = 0, 5
+MON, TUE, WED, THR, FRI, SAT, SUN = range(7)
+PM2, PM3 = 14, 15
+MIN_DAILY_LINKAGE = 2
+#
+THRESHOLD_VALUE = 30 * 60
