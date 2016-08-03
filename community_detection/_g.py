b7317f074fa76a4976c917dd56d6fc0bc66649fc
JerryHan88 <jerryhan88@gmail.com>
2016-08-03 11:53:55 +0800

    t

--- community_detection/_g.py
+++ community_detection/_g.py
@@ -13,25 +13,28 @@ def run():
     assert len(get_all_files(graph_dir, '', '.pkl')) == 1
     fn = get_all_files(graph_dir, '', '.pkl').pop()
     pairs_day_counting = load_pickle_file('%s/%s' % (graph_dir, fn))
+    print 'pkl file loading finished'
     G = nx.Graph()
     for (k0, k1), num_days in pairs_day_counting.iteritems():
         G.add_edge(k0, k1, weight=num_days)
 
     # first compute the best partition
     partition = community.best_partition(G)
-
+    print 'Partitioning finished'
     # drawing
     size = float(len(set(partition.values())))
     pos = nx.spring_layout(G)
     count = 0.
     for com in set(partition.values()):
         count = count + 1.
+        print count
         list_nodes = [nodes for nodes in partition.keys()
                       if partition[nodes] == com]
         nx.draw_networkx_nodes(G, pos, list_nodes, node_size=20,
                                node_color=str(count / size), with_labels=True)
-
+    print 'node drawing finished'
     nx.draw_networkx_edges(G, pos, alpha=0.5)
+    print 'edge drawing finished'
     plt.show()
 
 
