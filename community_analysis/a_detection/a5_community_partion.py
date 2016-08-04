37585a2f576c5c8bab53191d0510b966f78bfad1
JerryHan88 <jerryhan88@gmail.com>
2016-08-04 15:04:44 +0800

    t

--- community_analysis/a_detection/a5_community_partion.py
+++ community_analysis/a_detection/a5_community_partion.py
@@ -55,4 +55,11 @@ def run():
 
 
 if __name__ == '__main__':
-    run()
+    from traceback import format_exc
+
+    try:
+        run()
+    except Exception as _:
+        with open('logging_Python.txt', 'w') as f:
+            f.write(format_exc())
+        raise
