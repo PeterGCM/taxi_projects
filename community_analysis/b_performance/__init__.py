768bd3fa9c9e86eb58b99f233ed69a73276d1694
JerryHan88 <jerryhan88@gmail.com>
2016-08-06 11:00:26 +0800

    t

--- community_analysis/b_performance/__init__.py
+++ community_analysis/b_performance/__init__.py
@@ -2,4 +2,4 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
 import community_analysis.__init__
 
-
+trip_prefix = 'trips-'
--- information_boards/a_overall_analysis/a1_intermediate/__init__.py
+++ information_boards/a_overall_analysis/a1_intermediate/__init__.py
@@ -2,4 +2,3 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
 #
 from a_overall_analysis import __init__
-
