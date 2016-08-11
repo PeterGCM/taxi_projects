46de94a351e52cc50398d5132f705bbb2dde1193
JerryHan88 <jerryhan88@gmail.com>
2016-08-11 16:26:09 +0800

    t

--- community_analysis/a_detection/a4_linkage_annual_uni.py
+++ community_analysis/a_detection/a4_linkage_annual_uni.py
@@ -8,11 +8,13 @@ from taxi_common.file_handling_functions import load_pickle_file, get_all_files,
 def run():
     yyyy = '2009'
     lm_yyyy_dir = lm_dir + '/%s' % yyyy
-    num_months, max_num_days = 0, -1e400
+    day_counting, max_num_days = 0, -1e400
     pairs_day_counting = {}
     N = set()
     for fn in get_all_files(lm_yyyy_dir, '', '.pkl'):
         lm = load_pickle_file('%s/%s' % (lm_yyyy_dir, fn))
+        _, D, _, _, _ = lm[:-len('.pkl')]
+        day_counting += int(D)
         print 'load', fn
         for (k0, k1), num_days in lm:
             N.add(k0); N.add(k1)
@@ -21,7 +23,7 @@ def run():
             pairs_day_counting[(k0, k1)] += num_days
             if max_num_days < pairs_day_counting[(k0, k1)]:
                 max_num_days = pairs_day_counting[(k0, k1)]
-    fn = '%s-N(%d)-E(%d)-maxD(%d).pkl' % (yyyy, len(N), len(pairs_day_counting), max_num_days)
+    fn = '%s-CD(%d)-MD(%d)-N(%d)-E(%d).pkl' % (yyyy, day_counting, max_num_days, len(N), len(pairs_day_counting))
     print 'Saving'
     save_pickle_file('%s/%s' % (la_dir, fn), pairs_day_counting)
 
