9650bc6bb917e014a587fa356c753df45a5a9689
JerryHan88 <jerryhan88@gmail.com>
2016-08-11 16:26:58 +0800

    t

--- community_analysis/a_detection/a4_linkage_annual_uni.py
+++ community_analysis/a_detection/a4_linkage_annual_uni.py
@@ -13,7 +13,7 @@ def run():
     N = set()
     for fn in get_all_files(lm_yyyy_dir, '', '.pkl'):
         lm = load_pickle_file('%s/%s' % (lm_yyyy_dir, fn))
-        _, D, _, _, _ = lm[:-len('.pkl')]
+        _, D, _, _, _ = lm[:-len('.pkl')].split('-')
         day_counting += int(D)
         print 'load', fn
         for (k0, k1), num_days in lm:
