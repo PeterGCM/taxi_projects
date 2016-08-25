3fe5a652ce8b40ad386497421338ee2768411f0e
JerryHan88 <jerryhan88@gmail.com>
2016-08-25 16:11:00 +0800

    t

---
+++ information_boards/a_overall_analysis/a1_intermediate/a1_i2_filtering_specific_period.py
@@ -0,0 +1,31 @@
+import __init__
+#
+from information_boards.__init__ import error_period
+from a_overall_analysis.__init__ import trips_dir, trip_prefix
+#
+import csv, datetime
+
+
+def run():
+    for ys, ms, ds, hs in error_period:
+        yymm = '%02d%02d' % (int(ys), int(ms))
+        dd, hh = int(ds), int(hs)
+        fpath = '%s/%s%s.csv' % (trips_dir, trip_prefix, yymm)
+        temp_fpath = '%s/%s%s.csv.tmp' % (trips_dir, trip_prefix, yymm)
+        with open(fpath, 'rb') as r_csvfile1:
+            reader = csv.reader(r_csvfile1)
+            headers = reader.next()
+            hid = {h: i for i, h in enumerate(headers)}
+            with open(temp_fpath, 'wb') as w_csvfile:
+                writer = csv.writer(w_csvfile)
+                writer.writerow(headers)
+                for row in reader:
+                    t = eval(row[hid['start-time']])
+                    cur_dt = datetime.datetime.fromtimestamp(t)
+                    if cur_dt.day == dd and cur_dt.hour == hh:
+                        continue
+                    writer.writerow(row)
+
+
+if __name__ == '__main__':
+    run()
