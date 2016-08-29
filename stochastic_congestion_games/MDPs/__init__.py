e9d55149fbd7fa76168c6b7ce06c30c5a796e10a
jerryhan88 <jerryhan88@gmail.com>
2016-08-29 16:42:36 +0800

    Delete __init__.py

--- stochastic_congestion_games/MDPs/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import division
-import os, sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
-import stochastic_congestion_games.__init__
-
-policy_prefix, x_prefix, dist_prefix, lp_prefix = 'policy-', 'x-', 'dist-', 'LP-MODEL-'
