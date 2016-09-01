23bb8dcbef92f19cae1ee38d2b08280289929f2a
JerryHan88 <jerryhan88@gmail.com>
2016-09-01 16:06:07 +0800

    t

--- community_analysis/a_detection/a1_trip.py
+++ community_analysis/a_detection/a1_trip.py
@@ -27,7 +27,7 @@ def run():
 
 def process_file(yymm):
     ft_drivers = load_pickle_file('%s/%s%s.pkl' % (full_time_driver_dir, ft_drivers_prefix, yymm))
-    print ft_drivers
+    print len(ft_drivers)
     assert False
     yymm_dir = trip_dir + '/%s' % yymm
     remove_create_dir(yymm_dir)
