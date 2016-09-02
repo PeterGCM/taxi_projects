2e3aef2e0462175ea56f461a56296706d0617114
JerryHan88 <jerryhan88@gmail.com>
2016-09-02 12:46:50 +0800

    t

--- community_analysis/a_detection/a4_linkage_annual_uni.py
+++ community_analysis/a_detection/a4_linkage_annual_uni.py
@@ -1,5 +1,6 @@
 import __init__
 #
+from community_analysis.a_detection import REMAINING_LINKAGE_RATIO
 from community_analysis import lm_dir, la_dir
 #
 from taxi_common.file_handling_functions import load_pickle_file, get_all_files, \
@@ -10,26 +11,31 @@ def run():
     remove_create_dir(la_dir)
     #
     yyyy = '2009'
-    day_counting, max_num_days = 0, -1e400
+    day_counting = 0
     pairs_day_counting = {}
-    N = set()
+
     for fn in get_all_files(lm_dir, '', '.pkl'):
         _, D, _, _, _ = fn[:-len('.pkl')].split('-')
         day_counting += int(D[len('D('):-len(')')])
         lm = load_pickle_file('%s/%s' % (lm_dir, fn))
         print 'load', fn
         for (k0, k1), num_days in lm:
-            N.add(k0); N.add(k1)
             if (k0, k1) not in pairs_day_counting:
                 pairs_day_counting[(k0, k1)] = 0
             pairs_day_counting[(k0, k1)] += num_days
-            if max_num_days < pairs_day_counting[(k0, k1)]:
-                max_num_days = pairs_day_counting[(k0, k1)]
 
-
-    fn = '%s-CD(%d)-MD(%d)-N(%d)-E(%d).pkl' % (yyyy, day_counting, max_num_days, len(N), len(pairs_day_counting))
+    th_day = int(day_counting * REMAINING_LINKAGE_RATIO)
+    filtered_pairs = {}
+    N = set()
+    for (k0, k1), num_days in pairs_day_counting.iteritems():
+        if num_days < th_day:
+            continue
+        N.add(k0); N.add(k1)
+        filtered_pairs[(k0, k1)] += num_days
+    #
+    fn = '%s-CD(%d)-thD(%d)-N(%d)-E(%d).pkl' % (yyyy, day_counting, th_day, len(N), len(filtered_pairs))
     print 'Saving'
-    save_pickle_file('%s/%s' % (la_dir, fn), pairs_day_counting)
+    save_pickle_file('%s/%s' % (la_dir, fn), filtered_pairs)
 
 
 if __name__ == '__main__':
