42958d195f7dc88e05ab91f2386486379d6282e9
JerryHan88 <jerryhan88@gmail.com>
2016-09-11 13:23:23 +0800

    t

--- information_boards/a_overall_analysis/__init__.py
+++ information_boards/a_overall_analysis/__init__.py
@@ -2,10 +2,13 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../..')
 #
 from information_boards import taxi_data, summary_dir
+from taxi_common.file_handling_functions import check_dir_create
 #
 trips_dpath, trip_prefix = '%s/%s' % (taxi_data, 'trips'), 'trip-'
 #
-ap_tm_num_dur_fare_fpath = '%s/%s' % (summary_dir, 'ap-tm-num-dur-fare.csv')
-ns_tm_num_dur_fare_fpath = '%s/%s' % (summary_dir, 'ns-tm-num-dur-fare.csv')
+overall_summary_dptah = '%s/%s' % (summary_dir, 'overall_analysis')
+check_dir_create(overall_summary_dptah)
+ap_tm_num_dur_fare_fpath = '%s/%s' % (overall_summary_dptah, 'ap-tm-num-dur-fare.csv')
+ns_tm_num_dur_fare_fpath = '%s/%s' % (overall_summary_dptah, 'ns-tm-num-dur-fare.csv')
 #
 NUM, DUR, FARE = range(3)
