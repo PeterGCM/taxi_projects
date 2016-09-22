a6548818940b18959ba47678d8814c931f002c1d
JerryHan88 <jerryhan88@gmail.com>
2016-09-22 17:05:53 +0800

    MDP_agents

--- taxi_common/geo_functions.py
+++ taxi_common/geo_functions.py
@@ -50,7 +50,7 @@ def generate_zones(poly_points, xaxis_unit, yaxis_unit, x_points, y_points):
                 boundary_relation = zone.OUT
             zones[(i, j)] = zone(boundary_relation, i, j, cCoor_gps, polyPoints_gps)
             feature = {"type":"Feature",
-                       "id": '%d@%d' % (i,j),
+                       "id": '%03d%03d' % (i,j),
                        "properties": {"cCoor_gps": cCoor_gps},
                        "geometry":
                            {"type": "Polygon",
