853d2bd4a9f592982c04b0e277781567b843e000
JerryHan88 <jerryhan88@gmail.com>
2016-10-07 12:46:13 +0800

    t

--- community_analysis/a_preprocessing/a3_tf_zone_distribution.py
+++ community_analysis/a_preprocessing/a3_tf_zone_distribution.py
@@ -8,9 +8,10 @@ from taxi_common.file_handling_functions import check_dir_create, check_path_exi
 
 def run():
     check_dir_create(tf_zone_distribution_dir)
-
+    #
     for y in range(9, 13):
         yyyy = '20%02d' % y
+        print 'Handle %s' % yyyy
         year_distribution_fpath = '%s/%s%s.pkl' % (tf_zone_distribution_dir, tf_zone_distribution_prefix, yyyy)
         if check_path_exist(year_distribution_fpath):
             continue
@@ -21,6 +22,7 @@ def run():
             year_counting = {}
             for m in range(1, 13):
                 yymm = '%02d%02d' % (yymm)
+                print 'Handle the file; %s' % yymm
                 tf_zone_counting_fpath = '%s/%s%s.pkl' % (tf_zone_counting_dir, tf_zone_counting_prefix, yymm)
                 if not check_path_exist(tf_zone_counting_fpath):
                     continue
