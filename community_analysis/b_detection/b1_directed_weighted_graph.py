b6182298a96525e9fc7fa8c2f82baf30d7bd170a
JerryHan88 <jerryhan88@gmail.com>
2016-10-08 11:32:46 +0800

    t

--- community_analysis/b_detection/b1_directed_weighted_graph.py
+++ community_analysis/b_detection/b1_directed_weighted_graph.py
@@ -2,52 +2,80 @@ import __init__
 #
 from community_analysis import tf_zone_distribution_dir, tf_zone_distribution_prefix
 from community_analysis import dw_graph_dir, dw_graph_prefix
-from community_analysis import ft_trips_dir
+from community_analysis import ft_trips_dir, ft_trips_prefix
 from community_analysis._classes import ca_driver, ca_zone
 #
-from taxi_common.file_handling_functions import check_path_exist, load_pickle_file, get_all_files, save_pickle_file
+from taxi_common.file_handling_functions import check_path_exist, load_pickle_file, save_pickle_file, check_dir_create
+from taxi_common.multiprocess import init_multiprocessor, put_task, end_multiprocessor
 from taxi_common.sg_grid_zone import get_sg_zones
 #
-import csv
+import os, logging
+import csv, datetime
 
 
 def run():
+    check_dir_create(dw_graph_dir)
+    #
+    init_multiprocessor(6)
+    count_num_jobs = 0
     for y in range(9, 13):
-        yyyy = '20%02d' % y
-        print 'Handle %s' % yyyy
-        year_dw_graph_fpath = '%s/%s%s.pkl' % (dw_graph_dir, dw_graph_prefix, yyyy)
-        if check_path_exist(year_dw_graph_fpath):
-            print 'The file had already been processed; %s' % yyyy
-            continue
-        year_distribution_fpath = '%s/%s%s.pkl' % (tf_zone_distribution_dir, tf_zone_distribution_prefix, yyyy)
-        year_distribution = load_pickle_file(year_distribution_fpath)
-        for fn in get_all_files(ft_trips_dir, '', '.csv'):
-            _, _, _, yymm = fn[:-len('.csv')].split('-')
-            if not yymm.startswith('%02d' % y):
+        for m in range(1, 12):
+            yymm = '%02d%02d' % (y, m)
+            put_task(process_file, [yymm])
+            count_num_jobs += 1
+    end_multiprocessor(count_num_jobs)
+
+
+def process_file(yymm):
+    ft_trips_fpath = '%s/%s%s.csv' % (ft_trips_dir, ft_trips_prefix, yymm)
+    if not check_path_exist(ft_trips_fpath):
+        return None
+    year_dw_graph_fpath = '%s/%s%s.pkl' % (dw_graph_dir, dw_graph_prefix, yymm)
+    if check_path_exist(year_dw_graph_fpath):
+        print 'The file had already been processed; %s' % yymm
+        None
+    #
+    logger = logging.getLogger('dw_graph_%d' % os.getpid())
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('dw_graph_%d.log' % os.getpid())
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.info('Start %s directed weighted graph processing' % yymm)
+    #
+    yyyy = '20%s' % yymm[:2]
+    year_distribution_fpath = '%s/%s%s.pkl' % (tf_zone_distribution_dir, tf_zone_distribution_prefix, yyyy)
+    year_distribution = load_pickle_file(year_distribution_fpath)
+    logger.info('Finish year distribution loading')
+    #
+    drivers = {}
+    zones = generate_zones()
+    handling_day = 0
+    with open(ft_trips_fpath, 'rb') as r_csvfile:
+        reader = csv.reader(r_csvfile)
+        headers = reader.next()
+        hid = {h: i for i, h in enumerate(headers)}
+        for row in reader:
+            t = eval(row[hid['time']])
+            cur_dt = datetime.datetime.fromtimestamp(t)
+            if handling_day != cur_dt.day:
+                logger.info('Processing %dth day' % cur_dt.day)
+                handling_day = cur_dt.day
+            did = int(row[hid['did']])
+            zi, zj = int(row[hid['zi']]), int(row[hid['zj']])
+            try:
+                z = zones[(zi, zj)]
+            except KeyError:
                 continue
-            drivers = {}
-            zones = generate_zones()
-            print 'Handling %s' % yymm
-            with open('%s/%s' % (ft_trips_dir, fn), 'rb') as r_csvfile:
-                reader = csv.reader(r_csvfile)
-                headers = reader.next()
-                hid = {h: i for i, h in enumerate(headers)}
-                for row in reader:
-                    t = eval(row[hid['time']])
-                    did = int(row[hid['did']])
-                    zi, zj = int(row[hid['zi']]), int(row[hid['zj']])
-                    try:
-                        z = zones[(zi, zj)]
-                    except KeyError:
-                        continue
-                    if not drivers.has_key(did):
-                        drivers[did] = ca_driver(did, year_distribution[did])
-                    drivers[did].update_linkage(t, z)
-        print 'Aggregation %s' % yyyy
-        year_dw_graph = []
-        for did, d in drivers.iteritems():
-            year_dw_graph.append((did, d.num_pickup, d.weighted_link))
-        save_pickle_file(year_dw_graph_fpath, year_dw_graph)
+            if not drivers.has_key(did):
+                drivers[did] = ca_driver(did, year_distribution[did])
+            drivers[did].update_linkage(t, z)
+    logger.info('Start %s aggregation' % yymm)
+    year_dw_graph = []
+    for did, d in drivers.iteritems():
+        year_dw_graph.append((did, d.num_pickup, d.weighted_link))
+    save_pickle_file(year_dw_graph_fpath, year_dw_graph)
 
 
 def generate_zones():
@@ -56,6 +84,7 @@ def generate_zones():
     for k, z in basic_zones.iteritems():
         zones[k] = ca_zone(z.relation_with_poly, z.zi, z.zj, z.cCoor_gps, z.polyPoints_gps)
     return zones
+
 
 if __name__ == '__main__':
     run()
--- community_analysis/b_detection/b2_threshold_value_adjustment.py
+++ community_analysis/b_detection/b2_threshold_value_adjustment.py
@@ -1,35 +1,62 @@
 import __init__
 #
-from community_analysis import dw_graph_dir, dw_graph_prefix, dw_summary_fpath
+from community_analysis import ft_trips_dir, ft_trips_prefix
+from community_analysis import dw_graph_dir, dw_graph_prefix
+from community_analysis import dw_summary_fpath
 #
-from taxi_common.file_handling_functions import check_path_exist, load_pickle_file, get_all_files, save_pickle_file
+from taxi_common.file_handling_functions import load_pickle_file
 from taxi_common.charts import one_histogram
 #
+import numpy as np
+import scipy.stats.stats as st
 import csv
+#
+NUM_BINS = 40
 
 
 def run():
-    year_dw_summary_fpath = '%s/%s%s.csv' % (dw_graph_dir, dw_summary_prefix, yyyy)
-    if check_path_exist(year_dw_summary_fpath):
-        print 'The file had already been processed; %s' % yyyy
-        continue
-
-    with open(year_dw_summary_fpath, 'wt') as w_csvfile:
+    with open(dw_summary_fpath, 'wt') as w_csvfile:
         writer = csv.writer(w_csvfile, lineterminator='\n')
-        writer.writerow(['time', 'did',
-                         'day', 'timeFrame', 'zi', 'zj',
-                         'distance', 'duration', 'fare'])
+        writer.writerow(['year', 'days','numDrivers',
+                         'numPickupsTotal', 'numPickupsAverage', 'numPickupsSD',
+                            'numPickupsMin', 'numPickupsMax', 'numPickupsSkew',
+                         'weightTotal', 'weightAverage', 'weightSD',
+                            'weightMin', 'weightMax', 'weightSkew'])
     for y in range(9, 13):
         yyyy = '20%02d' % y
         print 'Handle %s' % yyyy
-
         year_dw_graph_fpath = '%s/%s%s.pkl' % (dw_graph_dir, dw_graph_prefix, yyyy)
         year_dw_graph = load_pickle_file(year_dw_graph_fpath)
-
-
-
-
-    pass
+        num_drivers = 0
+        pickups, weights = [], []
+        for did, num_pickup, weighted_link in year_dw_graph:
+            num_drivers += 1
+            pickups.append(num_pickup)
+            weights += weighted_link.values()
+        pickups_np, weights_np = np.asarray(pickups), np.asarray(weights)
+        month_day = set()
+        for m in range(1, 12):
+            yymm = '%02d%02d' % (y, m)
+            ft_trips_fpath = '%s/%s%s.csv' % (ft_trips_dir, ft_trips_prefix, yymm)
+            with open(ft_trips_fpath, 'rb') as r_csvfile:
+                reader = csv.reader(r_csvfile)
+                headers = reader.next()
+                hid = {h: i for i, h in enumerate(headers)}
+                for row in reader:
+                    day = row[hid['day']]
+                    month_day.add((m, day))
+        with open(dw_summary_fpath, 'a') as w_csvfile:
+            writer = csv.writer(w_csvfile, lineterminator='\n')
+            writer.writerow([yyyy, len(month_day), num_drivers,
+                             pickups_np.sum(), pickups_np.mean(), pickups_np.std(),
+                                pickups_np.min(), pickups_np.max(), st.skew(pickups_np),
+                             weights_np.sum(), weights_np.mean(), weights_np.std(),
+                             weights_np.min(), pickups_np.max(), st.skew(weights_np)])
+        #
+        one_histogram((6, 6), '', '# of pickups', 'Probability', NUM_BINS,
+                        pickups_np, '%s/num-pickups-%s' % (dw_graph_dir, yyyy))
+        one_histogram((6, 6), '', 'weights', 'Probability', NUM_BINS,
+                      weights_np, '%s/weights-%s' % (dw_graph_dir, yyyy))
 
 
 if __name__ == '__main__':
