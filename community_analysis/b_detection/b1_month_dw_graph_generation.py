d1baedc36299f6dfa7e27947ef2b20865bbd7006
JerryHan88 <jerryhan88@gmail.com>
2016-10-08 20:04:22 +0800

    t

--- community_analysis/b_detection/b1_month_dw_graph_generation.py
+++ community_analysis/b_detection/b1_month_dw_graph_generation.py
@@ -17,7 +17,7 @@ logger = get_logger('dw_graph')
 def run():
     check_dir_create(dw_graph_dir)
     #
-    init_multiprocessor(6)
+    init_multiprocessor(4)
     count_num_jobs = 0
     for y in range(9, 13):
         for m in range(1, 12):
