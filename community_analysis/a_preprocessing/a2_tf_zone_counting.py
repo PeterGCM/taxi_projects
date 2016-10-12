7e03dd980c6841fcb0d8cc50bcd35225443fdf15
JerryHan88 <jerryhan88@gmail.com>
2016-10-12 08:47:18 +0800

    t

--- community_analysis/a_preprocessing/a2_tf_zone_counting.py
+++ community_analysis/a_preprocessing/a2_tf_zone_counting.py
@@ -62,4 +62,11 @@ def process_file(yymm):
 
 
 if __name__ == '__main__':
-    run()
+    from traceback import format_exc
+    #
+    try:
+        run()
+    except Exception as _:
+        with open('Exception tf zone counting.txt', 'w') as f:
+            f.write(format_exc())
+        raise
