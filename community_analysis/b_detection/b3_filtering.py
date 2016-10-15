99d693ade20c40017cc09e08a6f399590d885f23
JerryHan88 <jerryhan88@gmail.com>
2016-10-15 15:46:09 +0800

    t

--- community_analysis/b_detection/b3_filtering.py
+++ community_analysis/b_detection/b3_filtering.py
@@ -67,4 +67,11 @@ def handle_year(dw_year_fn, year_dw_graph):
 
 
 if __name__ == '__main__':
-    run()
+    from traceback import format_exc
+    #
+    try:
+        run()
+    except Exception as _:
+        with open('Exception filtering.txt', 'w') as f:
+            f.write(format_exc())
+        raise
