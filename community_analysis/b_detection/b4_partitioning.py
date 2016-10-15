336e471f2c7c163704ce04572dbcb752267f45e4
JerryHan88 <jerryhan88@gmail.com>
2016-10-15 22:01:28 +0800

    t

--- community_analysis/b_detection/b4_partitioning.py
+++ community_analysis/b_detection/b4_partitioning.py
@@ -68,4 +68,11 @@ def process_file(dw_per_fpath, dw_per_fn):
 
 
 if __name__ == '__main__':
-    run()
+    from traceback import format_exc
+    #
+    try:
+        run()
+    except Exception as _:
+        with open('Exception filtering.txt', 'w') as f:
+            f.write(format_exc())
+        raise
