87b2783d1b89a0fe2a9d982714f31f11cfa8e83b
JerryHan88 <jerryhan88@gmail.com>
2016-10-17 09:46:51 +0800

    t

--- community_analysis/report/helping_functions.py
+++ community_analysis/report/helping_functions.py
@@ -1,6 +1,6 @@
 import __init__
 #
-from community_analysis import com_summary_2009_fpath
+# from community_analysis import com_summary_2009_fpath
 #
 from taxi_common.file_handling_functions import check_path_exist, save_pickle_file, get_all_directories, get_all_files
 #
@@ -67,37 +67,37 @@ def draw_grid_on_map(map_osm, x_points, y_points):
     return map_osm
 
 
-def get_com_stats_summary():
-    if not check_path_exist(com_summary_2009_fpath):
-        from community_analysis import com_dir
-        headers = ['Threshold value (Day)', 'Num of community', 'Num of drivers',
-                   'Average', 'Median', 'SD', 'Skewness', 'Kurtosis',
-                   'List of communities, (community ID, # of drivers)']
-        df_data = {k: [] for k in headers}
-        L_THD, L_NC, L_ND, L_A, L_M, L_SD, L_SK, L_KU, L_LC = range(len(headers))
-
-        #
-        for dn in get_all_directories(com_dir):
-            dpath = '%s/%s' % (com_dir, dn)
-            summary_fn = get_all_files(dpath, '', '.csv').pop()
-            _, _, str_thD, _, _ = summary_fn[:-len('.csv')].split('-')
-            thD = int(str_thD[len('thD('):-len(')')])
-            df = pd.read_csv('%s/%s' % (dpath, summary_fn))
-            df_data[headers[L_THD]].append(thD)
-            df_data[headers[L_NC]].append(df['num-nodes'].count())
-            df_data[headers[L_ND]].append(df['num-nodes'].sum())
-            df_data[headers[L_A]].append(df['num-nodes'].mean())
-            df_data[headers[L_M]].append(df['num-nodes'].median())
-            df_data[headers[L_SD]].append(df['num-nodes'].std())
-            df_data[headers[L_SK]].append(df['num-nodes'].skew())
-            df_data[headers[L_KU]].append(df['num-nodes'].kurt())
-            df_data[headers[L_LC]].append(','.join(['{%s:%d}' % (cname, num_nodes)
-                                                for cname, num_nodes in df.loc[:, ['com-name', 'num-nodes']].values]))
-        df = pd.DataFrame(df_data)[headers]
-        df.to_csv(com_summary_2009_fpath, index=False)
-        return df
-    else:
-        return pd.read_csv(com_summary_2009_fpath)
+# def get_com_stats_summary():
+#     if not check_path_exist(com_summary_2009_fpath):
+#         from community_analysis import com_dir
+#         headers = ['Threshold value (Day)', 'Num of community', 'Num of drivers',
+#                    'Average', 'Median', 'SD', 'Skewness', 'Kurtosis',
+#                    'List of communities, (community ID, # of drivers)']
+#         df_data = {k: [] for k in headers}
+#         L_THD, L_NC, L_ND, L_A, L_M, L_SD, L_SK, L_KU, L_LC = range(len(headers))
+#
+#         #
+#         for dn in get_all_directories(com_dir):
+#             dpath = '%s/%s' % (com_dir, dn)
+#             summary_fn = get_all_files(dpath, '', '.csv').pop()
+#             _, _, str_thD, _, _ = summary_fn[:-len('.csv')].split('-')
+#             thD = int(str_thD[len('thD('):-len(')')])
+#             df = pd.read_csv('%s/%s' % (dpath, summary_fn))
+#             df_data[headers[L_THD]].append(thD)
+#             df_data[headers[L_NC]].append(df['num-nodes'].count())
+#             df_data[headers[L_ND]].append(df['num-nodes'].sum())
+#             df_data[headers[L_A]].append(df['num-nodes'].mean())
+#             df_data[headers[L_M]].append(df['num-nodes'].median())
+#             df_data[headers[L_SD]].append(df['num-nodes'].std())
+#             df_data[headers[L_SK]].append(df['num-nodes'].skew())
+#             df_data[headers[L_KU]].append(df['num-nodes'].kurt())
+#             df_data[headers[L_LC]].append(','.join(['{%s:%d}' % (cname, num_nodes)
+#                                                 for cname, num_nodes in df.loc[:, ['com-name', 'num-nodes']].values]))
+#         df = pd.DataFrame(df_data)[headers]
+#         df.to_csv(com_summary_2009_fpath, index=False)
+#         return df
+#     else:
+#         return pd.read_csv(com_summary_2009_fpath)
 
 
 def generate_3D_graph(labels, group, layt, Edges):
