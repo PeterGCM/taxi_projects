0155c1a57ebe2f9347495ffe0397fab8a57b4605
JerryHan88 <jerryhan88@gmail.com>
2016-10-24 14:29:18 +0800

    t

--- community_analysis/a_preprocessing/a2_year_trips_merge_months.py
+++ community_analysis/a_preprocessing/a2_year_trips_merge_months.py
@@ -12,7 +12,7 @@ logger = get_logger('year_trip_merge_months')
 
 def run():
     #
-    for y in range(9, 13):
+    for y in range(9, 10):
         ss_year_trips_fpath = '%s/%s20%02d.csv' % (ss_trips_dir, ss_trips_prefix, y)
         if check_path_exist(ss_year_trips_fpath):
             continue
@@ -34,54 +34,59 @@ def run():
                         writer.writerow(row)
 
 
+
+
 if __name__ == '__main__':
-    import time
-    logger.info('Execution')
-    MODI_TH = 5 * 60
-    while True:
-        Y09 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 9), '.csv')
-        Y10 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 10), '.csv')
-        Y11 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 11), '.csv')
-        Y12 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 12), '.csv')
-        if len(Y09) == 11:
-            last_modi_time = -1e400
-            for fn in Y09:
-                fpath = '%s/%s' % (ss_trips_dir, fn)
-                if last_modi_time < get_modified_time(fpath):
-                    last_modi_time = get_modified_time(fpath)
-            if time.time() - last_modi_time > MODI_TH:
-                run()
-        if len(Y10) == 11:
-            last_modi_time = -1e400
-            for fn in Y10:
-                fpath = '%s/%s' % (ss_trips_dir, fn)
-                if last_modi_time < get_modified_time(fpath):
-                    last_modi_time = get_modified_time(fpath)
-            if time.time() - last_modi_time > MODI_TH:
-                run()
-        if len(Y11) == 7:
-            last_modi_time = -1e400
-            for fn in Y11:
-                fpath = '%s/%s' % (ss_trips_dir, fn)
-                if last_modi_time < get_modified_time(fpath):
-                    last_modi_time = get_modified_time(fpath)
-            if time.time() - last_modi_time > MODI_TH:
-                run()
-        if len(Y12) == 9:
-            last_modi_time = -1e400
-            for fn in Y12:
-                fpath = '%s/%s' % (ss_trips_dir, fn)
-                if last_modi_time < get_modified_time(fpath):
-                    last_modi_time = get_modified_time(fpath)
-            if time.time() - last_modi_time > MODI_TH:
-                run()
-        for y in range(9, 13):
-            ss_year_trips_fpath = '%s/%s20%02d.csv' % (ss_trips_dir, ss_trips_prefix, y)
-            if not check_path_exist(ss_year_trips_fpath):
-                break
-        else:
-            break
-        logger.info('Falling sleep')
-        time.sleep(20 * 60)
+    run()
+
+
+    # import time
+    # logger.info('Execution')
+    # MODI_TH = 5 * 60
+    # while True:
+    #     Y09 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 9), '.csv')
+    #     Y10 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 10), '.csv')
+    #     Y11 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 11), '.csv')
+    #     Y12 = get_all_files(ss_trips_dir, '%s%02d' % (ss_trips_prefix, 12), '.csv')
+    #     if len(Y09) == 11:
+    #         last_modi_time = -1e400
+    #         for fn in Y09:
+    #             fpath = '%s/%s' % (ss_trips_dir, fn)
+    #             if last_modi_time < get_modified_time(fpath):
+    #                 last_modi_time = get_modified_time(fpath)
+    #         if time.time() - last_modi_time > MODI_TH:
+    #             run()
+    #     if len(Y10) == 11:
+    #         last_modi_time = -1e400
+    #         for fn in Y10:
+    #             fpath = '%s/%s' % (ss_trips_dir, fn)
+    #             if last_modi_time < get_modified_time(fpath):
+    #                 last_modi_time = get_modified_time(fpath)
+    #         if time.time() - last_modi_time > MODI_TH:
+    #             run()
+    #     if len(Y11) == 7:
+    #         last_modi_time = -1e400
+    #         for fn in Y11:
+    #             fpath = '%s/%s' % (ss_trips_dir, fn)
+    #             if last_modi_time < get_modified_time(fpath):
+    #                 last_modi_time = get_modified_time(fpath)
+    #         if time.time() - last_modi_time > MODI_TH:
+    #             run()
+    #     if len(Y12) == 9:
+    #         last_modi_time = -1e400
+    #         for fn in Y12:
+    #             fpath = '%s/%s' % (ss_trips_dir, fn)
+    #             if last_modi_time < get_modified_time(fpath):
+    #                 last_modi_time = get_modified_time(fpath)
+    #         if time.time() - last_modi_time > MODI_TH:
+    #             run()
+    #     for y in range(9, 13):
+    #         ss_year_trips_fpath = '%s/%s20%02d.csv' % (ss_trips_dir, ss_trips_prefix, y)
+    #         if not check_path_exist(ss_year_trips_fpath):
+    #             break
+    #     else:
+    #         break
+    #     logger.info('Falling sleep')
+    #     time.sleep(20 * 60)
 
 
