8bb598b65cb7eea20b18a543d2a22d9c58e4d6da
JerryHan88 <jerryhan88@gmail.com>
2016-10-27 17:37:34 +0800

    t

--- community_analysis/b_detection/b4_partitioning.py
+++ community_analysis/b_detection/b4_partitioning.py
@@ -19,7 +19,8 @@ def run():
     init_multiprocessor(3)
     count_num_jobs = 0
     dw_per_dirs = get_all_directories(fdw_graph_dir)
-    for dw_per_dir_fn in dw_per_dirs.reverse():
+    dw_per_dirs.reverse()
+    for dw_per_dir_fn in dw_per_dirs:
         dw_per_dirpath = '%s/%s' % (fdw_graph_dir, dw_per_dir_fn)
         for dw_per_fn in get_all_files(dw_per_dirpath, '', '.pkl'):
             dw_per_fpath = '%s/%s' % (dw_per_dirpath, dw_per_fn)
