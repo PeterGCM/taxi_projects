caa387bc3c876668ac6411b62b894a7fc2be66ff
JerryHan88 <jerryhan88@gmail.com>
2016-11-05 13:31:34 +0800

    t

--- information_boards/c_individual_analysis/c1_intermediate/c1_i3_daily_drivers_statistics.py
+++ information_boards/c_individual_analysis/c1_intermediate/c1_i3_daily_drivers_statistics.py
@@ -36,54 +36,56 @@ def process_files(yymm):
     print 'handle the file; %s' % yymm
     #
     shift_df = pd.read_csv('%s/%s%s.csv' % (ftd_shift_dir, ftd_shift_prefix, yymm))
-    
-
-    
     all_trip_df = pd.read_csv('%s/%s%s.csv' % (ftd_trips_dir, ftd_trips_prefix, yymm))
     loc_trip_df = pd.read_csv('%s/%s%s.csv' % (ap_ep_dir, ap_ep_prefix, yymm))
     ft_drivers = map(int, load_pickle_file('%s/%s%s.pkl' % (full_time_driver_dir, ft_drivers_prefix, yymm)))
+    days = set(loc_trip_df['dd'])
     #
     yy, mm = int(yymm[:2]), int(yymm[2:])
-    for did in ft_drivers:
-        #
-        # All
-        #
-        d_all_trip = all_trip_df[(all_trip_df['did'] == did)]
-        d_shift = shift_df[(shift_df['did'] == did)]
-        all_num = len(d_all_trip['fare'])
-        pro_dur = sum(d_shift['pro-dur']) * SEC60
-        all_fare = sum(d_all_trip['fare'])
-        #
-        # Specific location
-        #
-        d_loc_trip = loc_trip_df[(loc_trip_df['did'] == did)]
-        loc_num = len(d_loc_trip['fare'])
-        loc_dur = sum(d_loc_trip['duration'])
-        loc_fare = sum(d_loc_trip['fare'])
-        loc_ep = sum(d_loc_trip['economic-profit'])
-        loc_qtime = sum(d_loc_trip['queueing-time'])
-        #
-        d_loc_trip_in = d_loc_trip[(d_loc_trip['trip-mode'] == DIn_PIn)]
-        locIn_num = len(d_loc_trip_in['fare'])
-        locIn_dur = sum(d_loc_trip_in['duration'])
-        locIn_fare = sum(d_loc_trip_in['fare'])
-        locIn_ep = sum(d_loc_trip_in['economic-profit'])
-        locIn_qtime = sum(d_loc_trip_in['queueing-time'])
-        #
-        d_loc_trip_out = d_loc_trip[(d_loc_trip['trip-mode'] == DOut_PIn)]
-        locOut_num = len(d_loc_trip_out['fare'])
-        locOut_dur = sum(d_loc_trip_out['duration'])
-        locOut_fare = sum(d_loc_trip_out['fare'])
-        locOut_ep = sum(d_loc_trip_out['economic-profit'])
-        locOut_qtime = sum(d_loc_trip_out['queueing-time'])
-        #
-        with open(ftd_ap_daily_stat_fpath, 'a') as w_csvfile:
-            writer = csv.writer(w_csvfile, lineterminator='\n')
-            writer.writerow([yy, mm, did,
-                             all_num, pro_dur, all_fare,
-                             loc_num, loc_dur, loc_fare, loc_ep, loc_qtime,
-                             locIn_num, locIn_dur, locIn_fare, locIn_ep, locIn_qtime,
-                             locOut_num, locOut_dur, locOut_fare, locOut_ep, locOut_qtime])
+    for dd in days:
+        day_all_trip_df = all_trip_df[(all_trip_df['dd'] == dd)]
+        day_loc_trip_df = loc_trip_df[(loc_trip_df['dd'] == dd)]
+        day_shift_df = shift_df[(shift_df['dd'] == dd)]
+        for did in ft_drivers:
+            #
+            # All
+            #
+            d_all_trip = day_all_trip_df[(day_all_trip_df['did'] == did)]
+            d_shift = day_shift_df[(day_shift_df['did'] == did)]
+            all_num = len(d_all_trip['fare'])
+            pro_dur = sum(d_shift['pro-dur']) * SEC60
+            all_fare = sum(d_all_trip['fare'])
+            #
+            # Specific location
+            #
+            d_loc_trip = day_loc_trip_df[(day_loc_trip_df['did'] == did)]
+            loc_num = len(d_loc_trip['fare'])
+            loc_dur = sum(d_loc_trip['duration'])
+            loc_fare = sum(d_loc_trip['fare'])
+            loc_ep = sum(d_loc_trip['economic-profit'])
+            loc_qtime = sum(d_loc_trip['queueing-time'])
+            #
+            d_loc_trip_in = d_loc_trip[(d_loc_trip['trip-mode'] == DIn_PIn)]
+            locIn_num = len(d_loc_trip_in['fare'])
+            locIn_dur = sum(d_loc_trip_in['duration'])
+            locIn_fare = sum(d_loc_trip_in['fare'])
+            locIn_ep = sum(d_loc_trip_in['economic-profit'])
+            locIn_qtime = sum(d_loc_trip_in['queueing-time'])
+            #
+            d_loc_trip_out = d_loc_trip[(d_loc_trip['trip-mode'] == DOut_PIn)]
+            locOut_num = len(d_loc_trip_out['fare'])
+            locOut_dur = sum(d_loc_trip_out['duration'])
+            locOut_fare = sum(d_loc_trip_out['fare'])
+            locOut_ep = sum(d_loc_trip_out['economic-profit'])
+            locOut_qtime = sum(d_loc_trip_out['queueing-time'])
+            #
+            with open(ftd_ap_daily_stat_fpath, 'a') as w_csvfile:
+                writer = csv.writer(w_csvfile, lineterminator='\n')
+                writer.writerow([yy, mm, dd, did,
+                                 all_num, pro_dur, all_fare,
+                                 loc_num, loc_dur, loc_fare, loc_ep, loc_qtime,
+                                 locIn_num, locIn_dur, locIn_fare, locIn_ep, locIn_qtime,
+                                 locOut_num, locOut_dur, locOut_fare, locOut_ep, locOut_qtime])
 
 
 if __name__ == '__main__':
