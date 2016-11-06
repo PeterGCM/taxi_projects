a6dccee836240e4c62ed38eb59afa334cb43e990
JerryHan88 <jerryhan88@gmail.com>
2016-11-06 21:55:34 +0800

    t

--- community_analysis/d_validation/d3_tf_zone_drivers.py
+++ community_analysis/d_validation/d3_tf_zone_drivers.py
@@ -7,7 +7,7 @@ from community_analysis import X_APPEAR, O_APPEAR
 from community_analysis import PM2, PM11
 from community_analysis import CHOSEN_PERCENTILE
 #
-from taxi_common.file_handling_functions import check_dir_create, check_path_exist, load_pickle_file
+from taxi_common.file_handling_functions import check_dir_create, check_path_exist, load_pickle_file, save_pickle_file
 from taxi_common.sg_grid_zone import get_sg_zones
 from taxi_common.log_handling_functions import get_logger
 from taxi_common.multiprocess import init_multiprocessor, put_task, end_multiprocessor
@@ -38,6 +38,10 @@ def process_file(period):
     from traceback import format_exc
     #
     try:
+        tf_zone_drivers_fpath = '%s/%s%s.csv' % (tf_zone_drivers_dir, tf_zone_drivers_prefix, period)
+        if check_path_exist(tf_zone_drivers_fpath):
+            logger.info('Already handled; %s' % period)
+            return None
         group_drivers_fpath = '%s/%s/%s%s.pkl' % (com_drivers_dir, percentile_dirname, com_drivers_prefix, period)
         if not check_path_exist(group_drivers_fpath):
             logger.info('The file X exists; %s' % period)
@@ -52,8 +56,8 @@ def process_file(period):
             hid = {h: i for i, h in enumerate(headers)}
             row = reader.next()
             handling_day = int(row[hid['day']])
-            day_tf_zone_drivers = {}
-            tf_zone_drivers = {}
+            day_tf_zone_driver = {}
+            tf_zone_driver = {}
             while row:
                 day = int(row[hid['day']])
                 if handling_day != day:
@@ -63,16 +67,17 @@ def process_file(period):
                             for (zi, zj) in zones.iterkeys():
                                 if zi < 0 or zj < 0:
                                     continue
-                                day_tf_zone_drivers[handling_day, tf, zi, zj, com_did] = \
-                                    O_APPEAR if tf_zone_drivers.has_key((tf, zi, zj, com_did)) else X_APPEAR
+                                day_tf_zone_driver[handling_day, tf, zi, zj, com_did] = \
+                                    O_APPEAR if tf_zone_driver.has_key((tf, zi, zj, com_did)) else X_APPEAR
                     handling_day = day
-                    tf_zone_drivers = {}
+                    tf_zone_driver = {}
                 did = int(row[hid['did']])
                 tf = int(row[hid['timeFrame']])
                 zi, zj = int(row[hid['zi']]), int(row[hid['zj']])
-                tf_zone_drivers[tf, zi, zj, did] = O_APPEAR
+                tf_zone_driver[tf, zi, zj, did] = O_APPEAR
                 #
                 row = reader.next()
+        save_pickle_file(tf_zone_drivers_fpath, day_tf_zone_driver)
     except Exception as _:
         with open('tf_zone_drivers_%s.txt' % period, 'w') as f:
             f.write(format_exc())
