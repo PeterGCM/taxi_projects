0747cf2f3ebfc2f2619d2d71427d6c03aa386a6c
JerryHan88 <jerryhan88@gmail.com>
2016-11-23 14:44:27 +0800

    t

--- community_analysis/__init__.py
+++ community_analysis/__init__.py
@@ -30,7 +30,7 @@ dwg_summary = '%s/%s' % (dwg_dir, 'dwg-summary.csv')
 TOP5PERCENT = 0.05
 TOP1PERCENT = 0.01
 TOPZP5PERCENT = 0.005
-CHOSEN_PERCENT = TOP1PERCENT
+CHOSEN_PERCENT = TOPZP5PERCENT
 fdwg_dir = '%s/%s' % (taxi_data, 'fdw_graph(%.3f)' % CHOSEN_PERCENT); fdw_graph_prefix = 'fdw-graph-'
 
 group_dir = '%s/%s' % (taxi_data, 'group(%.2f)' % CHOSEN_PERCENT); group_prepix = 'group-'
