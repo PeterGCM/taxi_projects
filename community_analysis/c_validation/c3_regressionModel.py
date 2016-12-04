8807854a5892c3326585a5aa7576190afdf3a2e2
JerryHan88 <jerryhan88@gmail.com>
2016-12-04 17:52:23 +0800

    t

--- community_analysis/c_validation/c3_regressionModel.py
+++ community_analysis/c_validation/c3_regressionModel.py
@@ -27,19 +27,19 @@ def run():
         regressionModel_wc_dpath = '%s/%s' % (regressionModel_dpath, wc)
         check_dir_create(regressionModel_wc_dpath)
 
-    init_multiprocessor(6)
-    count_num_jobs = 0
-    for y in range(9, 10):
-        for m in range(1, 13):
-            yymm = '%02d%02d' % (y, m)
-            # process_file(yymm)
-            put_task(process_file, [yymm])
-            count_num_jobs += 1
-    end_multiprocessor(count_num_jobs)
-
+    # init_multiprocessor(6)
+    # count_num_jobs = 0
     # for y in range(9, 10):
-    #     yyyy = '20%02d' % (y)
-    #     merge_year(yyyy)
+    #     for m in range(1, 13):
+    #         yymm = '%02d%02d' % (y, m)
+    #         # process_file(yymm)
+    #         put_task(process_file, [yymm])
+    #         count_num_jobs += 1
+    # end_multiprocessor(count_num_jobs)
+
+    for y in range(9, 10):
+        yyyy = '20%02d' % (y)
+        merge_year(yyyy)
 
 
 def merge_year(period):
