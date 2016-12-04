81b05bd7f88b9cabe0d9214e5550c570854ff838
JerryHan88 <jerryhan88@gmail.com>
2016-12-04 09:05:40 +0800

    t

--- community_analysis/b_detection/b3_dw_graph_filtering.py
+++ community_analysis/b_detection/b3_dw_graph_filtering.py
@@ -21,7 +21,7 @@ logger = get_logger()
 def run():
     check_dir_create(fdwg_dpath)
     #
-    for y in range(10, 13):
+    for y in range(9, 10):
         yyyy = '20%02d' % y
         process_file(yyyy)
 
