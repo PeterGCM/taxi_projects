61a3aeb670fae7da7b9039d1c71b5f444b40b271
JerryHan88 <jerryhan88@gmail.com>
2016-12-07 15:18:51 +0800

    t

--- information_boards/c_individual_analysis/c1_intermediate/c1_i5_sensitivity_drivers.py
+++ information_boards/c_individual_analysis/c1_intermediate/c1_i5_sensitivity_drivers.py
@@ -21,36 +21,37 @@ def run():
     with open(ssd_apIn_fpath, 'wb') as w_csvfile:
         writer = csv.writer(w_csvfile, lineterminator='\n')
         headers = ['timeStamp', 'did', 'duration', 'fare',
-                   'year', 'month',' day', 'hour'
-                   'apIn', 'apQTime']
+                   'apIn', 'apQTime',
+                   'year', 'month',' day', 'hour']
         writer.writerow(headers)
-    for m in xrange(1, 13):
-        yymm = '10%02d' % m
-        if yymm in ['1010']:
-            continue
-        logger.info('Start handling; %s' % yymm)
-        ft_drivers = map(int, load_pickle_file('%s/%s%s.pkl' % (full_time_driver_dir, ft_drivers_prefix, yymm)))
-        ap_ep_fpath = '%s/%s%s.csv' % (ap_ep_dir, ap_ep_prefix, yymm)
-        with open(ap_ep_fpath, 'rb') as r_csvfile:
-            reader = csv.reader(r_csvfile)
-            headers = reader.next()
-            hid = {h: i for i, h in enumerate(headers)}
-            handling_day = 0
-            for row in reader:
-                did = int(row[hid['did']])
-                if did not in ft_drivers:
-                    continue
-                t = eval(row[hid['start-time']])
-                cur_dt = datetime.datetime.fromtimestamp(t)
-                if handling_day != cur_dt.day:
-                    logger.info('...ing; %s(%dth)' % (yymm, handling_day))
-                    handling_day = cur_dt.day
-                apIn = 1 if int(row[hid['trip-mode']]) == DIn_PIn else 0
-                new_row = [
-                    t, did, row[hid['duration']], row[hid['fare']],
-                    row[hid['yy']], row[hid['mm']], row[hid['dd']], row[hid['hh']],
-                    apIn, row[hid['queueing-time']]
-                ]
+        for m in xrange(1, 13):
+            yymm = '10%02d' % m
+            if yymm in ['1010']:
+                continue
+            logger.info('Start handling; %s' % yymm)
+            ft_drivers = map(int, load_pickle_file('%s/%s%s.pkl' % (full_time_driver_dir, ft_drivers_prefix, yymm)))
+            ap_ep_fpath = '%s/%s%s.csv' % (ap_ep_dir, ap_ep_prefix, yymm)
+            with open(ap_ep_fpath, 'rb') as r_csvfile:
+                reader = csv.reader(r_csvfile)
+                headers = reader.next()
+                hid = {h: i for i, h in enumerate(headers)}
+                handling_day = 0
+                for row in reader:
+                    did = int(row[hid['did']])
+                    if did not in ft_drivers:
+                        continue
+                    t = eval(row[hid['start-time']])
+                    cur_dt = datetime.datetime.fromtimestamp(t)
+                    if handling_day != cur_dt.day:
+                        logger.info('...ing; %s(%dth)' % (yymm, handling_day))
+                        handling_day = cur_dt.day
+                    apIn = 1 if int(row[hid['trip-mode']]) == DIn_PIn else 0
+                    new_row = [
+                        t, did, row[hid['duration']], row[hid['fare']],
+                        apIn, row[hid['queueing-time']],
+                        row[hid['yy']], row[hid['mm']], row[hid['dd']], row[hid['hh']]
+                    ]
+                    writer.writerow(new_row)
 
 
 if __name__ == '__main__':
