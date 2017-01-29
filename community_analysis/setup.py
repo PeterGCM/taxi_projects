3c880a906d6e90d61830193ea7e2f271e273a0ed
JerryHan88 <jerryhan88@gmail.com>
2017-01-29 16:26:14 +0800

    t

---
+++ community_analysis/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension("b_detection._cy_b1_influenceGraph", ["b_detection/_cy_b1_influenceGraph.pyx"], include_dirs=['.']),
+]
+
+setup(
+    name='taxi_projects',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+    script_args=['build_ext'],
+    options={'build_ext': {'inplace': True, 'force': True}}
+)
+
+print '********CYTHON COMPLETE******'
