2208309c2b15cd8a2b4fd2b07f4a67511a362588
JerryHan88 <jerryhan88@gmail.com>
2017-03-05 23:02:44 +0800

    t

--- community_analysis/a_TrajectoryAnalytics/run.sh
+++ community_analysis/a_TrajectoryAnalytics/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in 0{1..9} {10..12}; do
-    python -c "from a2_prevDriversDefined import process_month; process_month('12$i')" &
+    python -c "from a1_trip_ss_drivers import process_month; process_month('12$i')" &
 done
 
 #python -c "from a2_prevDriversDefined import filtering; filtering('2011')"
