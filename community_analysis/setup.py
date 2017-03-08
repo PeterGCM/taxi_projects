8ba8d39768eff08ce541aed0738efaa0aa2d001d
JerryHan88 <jerryhan88@gmail.com>
2017-03-08 15:06:58 +0800

    t

--- community_analysis/setup.py
+++ community_analysis/setup.py
@@ -2,17 +2,27 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [
-    Extension("f_Evolution.f2_regressionCommunityMembers",
-              ["f_Evolution/f2_regressionCommunityMembers.pyx"], include_dirs=['.']),
-]
+import sys
 
-setup(
-    name='taxi_projects',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules,
-    script_args=['build_ext'],
-    options={'build_ext': {'inplace': True, 'force': True}}
-)
+args = sys.argv
 
-print '********CYTHON COMPLETE******'
+if len(args) == len(['pyFile', 'packageName', 'fileName']):
+    _, packageName, fileName = args
+
+    ext_modules = [
+        Extension('%s.%s' % (packageName, fileName),
+                  ['%s/%s.pyx' % (packageName, fileName)], include_dirs=['.']),
+    ]
+
+    setup(
+        name='taxi_projects',
+        cmdclass={'build_ext': build_ext},
+        ext_modules=ext_modules,
+        script_args=['build_ext'],
+        options={'build_ext': {'inplace': True, 'force': True}}
+    )
+
+    print '******** CYTHON COMPLETE ******'
+else:
+    print '******** Error ******'
+    print '******** Type packageName and fileName ******'
