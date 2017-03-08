13dadb9977ebe2f7d7a651e62ac77a931d6b8282
JerryHan88 <jerryhan88@gmail.com>
2017-03-08 15:20:00 +0800

    t

--- community_analysis/setup.py
+++ community_analysis/setup.py
@@ -2,18 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-import sys
+import sys, platform
 
-args = sys.argv
 
-if len(args) == len(['pyFile', 'packageName', 'fileName']):
-    _, packageName, fileName = args
-
+def cythonize(packageName, fileName):
     ext_modules = [
         Extension('%s.%s' % (packageName, fileName),
                   ['%s/%s.pyx' % (packageName, fileName)], include_dirs=['.']),
     ]
-
     setup(
         name='taxi_projects',
         cmdclass={'build_ext': build_ext},
@@ -21,8 +17,25 @@ if len(args) == len(['pyFile', 'packageName', 'fileName']):
         script_args=['build_ext'],
         options={'build_ext': {'inplace': True, 'force': True}}
     )
-
     print '******** CYTHON COMPLETE ******'
+
+
+plf = platform.platform()
+if plf.startswith('Linux'):
+    # Linux server
+    args = sys.argv
+    if len(args) == len(['pyFile', 'packageName', 'fileName']):
+        _, packageName, fileName = args
+        cythonize(packageName, fileName)
+    else:
+        print '******** Error ******'
+        print '******** Type packageName and fileName ******'
+elif plf.startswith('Darwin'):
+    # Mac
+    packageName, fileName = 'a_TrajectoryAnalytics', 'a1_roamingNinterTravel'
+
+    cythonize(packageName, fileName)
 else:
-    print '******** Error ******'
-    print '******** Type packageName and fileName ******'
+    # Window ?
+    pass
+
