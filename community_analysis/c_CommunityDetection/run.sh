77efadd8865251b5d5935da5cf0930361effbc00
JerryHan88 <jerryhan88@gmail.com>
2017-03-12 17:05:25 +0800

    t

--- community_analysis/c_CommunityDetection/run.sh
+++ community_analysis/c_CommunityDetection/run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python -c "from c1_graphPartition import run; run()" &
+#python -c "from c1_graphPartition import run; run()" &
+
+for i in {0..6}; do
+    python -c "from c2_comTrips import run; run($i)" &
+done
