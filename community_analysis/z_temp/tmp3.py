43cbbfb93cd0efa2e1dcae2cb9c36cad70966f3e
JerryHan88 <jerryhan88@gmail.com>
2017-03-13 09:31:11 +0800

    t

--- community_analysis/z_temp/tmp3.py
+++ community_analysis/z_temp/tmp3.py
@@ -18,7 +18,7 @@ of_prefixs = prefixs[depVar, 'influenceGraph']
 countRelationWhole = {k: 0 for k in ['sigPos', 'sigNeg', 'XsigPos', 'XsigNeg']}
 
 for fn in get_all_files(of_dpath, '%scount-*' % of_prefixs):
-    print fpath
+    print fn
     fpath = '%s/%s' % (of_dpath, fn)
     countRelation = load_pickle_file(fpath)
     for n in ['sigPos', 'sigNeg', 'XsigPos', 'XsigNeg']:
