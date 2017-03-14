7ec00d29d5c963f94fc3561c0ae22dab7e5cfb07
JerryHan88 <jerryhan88@gmail.com>
2017-03-14 19:55:28 +0800

    t

--- information_boards/a_preprocessing/run.sh
+++ information_boards/a_preprocessing/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 for i in 0{1..9} {10..12}; do
-    python -c "from a6_hourProductivity import process_file; process_file('09$i')" &
+    python -c "from a6_hourProductivity import process_file; process_file('10$i')" &
 done
