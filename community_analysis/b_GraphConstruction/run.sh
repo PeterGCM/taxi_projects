f9b67e110882ad6652f4d970d0ff21b68d4034d8
ckhan <chungkyun.han@gmail.com>
2017-05-07 15:00:46 +0800

    t

--- community_analysis/b_GraphConstruction/run.sh
+++ community_analysis/b_GraphConstruction/run.sh
@@ -3,6 +3,6 @@
 #    python -c "from b1_priorPresence import run; run($i)" &
 #done
 
-for i in {0..2}; do
-    python -c "from b2_influenceGraph import run; run($i)" &
+for i in {0..5}; do
+    python -c "from b2_sigRelation import run; run($i)" &
 done
