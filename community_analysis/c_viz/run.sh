b4851c6a66275d5a48b254115f1893bb2a3afda4
ckhan <chungkyun.han@gmail.com>
2017-05-22 16:30:10 +0800

    t

--- community_analysis/c_viz/run.sh
+++ community_analysis/c_viz/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for i in 0{1..9} {10..12}; do
-    python -c "from c1_log_extraction import get_driver_log; get_driver_log('09$i', 1)" &
+    python -c "from c1_log_extraction import get_driver_log; get_driver_log('09$i', 32768)" &
 done
 
