a315c038ffa7c20f574b9814a902ce01e0ea532b
ckhan <chungkyun.han@gmail.com>
2017-05-24 15:57:32 +0800

    t

--- community_analysis/b_GraphConstruction/run.sh
+++ community_analysis/b_GraphConstruction/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#for i in {0..5}; do
-#    python -c "from b1_priorPresence import run; run($i)" &
-#done
-
 for i in {0..10}; do
-    python -c "from b4_individual import run; run($i)" &
+    python -c "from b1_priorPresence import run; run($i)" &
 done
+
+#for i in {0..10}; do
+#    python -c "from b4_individual import run; run($i)" &
+#done
