417d9e19c9bc648764c30624f289aa8f22b3ca73
ckhan <chungkyun.han@gmail.com>
2017-05-24 12:03:59 +0800

    t

--- community_analysis/a_TrajectoryAnalytics/run.sh
+++ community_analysis/a_TrajectoryAnalytics/run.sh
@@ -10,8 +10,6 @@
 #python -c "from a2_prevDrivers import roamingTimeFiltering; roamingTimeFiltering('2009')" &
 #python -c "from a2_prevDrivers import interTravelTimeFiltering; interTravelTimeFiltering('2009')" &
 
-
-
 for i in 0{1..9} {10..12}; do
     python -c "from a4_trip_extraction import get_drivers_trip; get_drivers_trip('09$i', [1, 32768])" &
 done
