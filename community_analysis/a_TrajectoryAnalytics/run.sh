57074523f00c92b36de1513ad4a1d1a4b485ac5c
ckhan <chungkyun.han@gmail.com>
2017-05-25 12:29:02 +0800

    t

--- community_analysis/a_TrajectoryAnalytics/run.sh
+++ community_analysis/a_TrajectoryAnalytics/run.sh
@@ -12,7 +12,7 @@
 
 
 for i in 0{1..9} {10..12}; do
-    python -c "from a3_log_extraction import get_driver_log; get_driver_log('09$i', 21340)" &
+    python -c "from a3_log_extraction import get_driver_log; get_driver_log('09$i', 17742)" &
 done
 
 
