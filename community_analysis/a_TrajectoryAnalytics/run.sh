851aeacdb919ba924915c8f391956433a73195f1
ckhan <chungkyun.han@gmail.com>
2017-05-26 07:46:37 +0800

    t

--- community_analysis/a_TrajectoryAnalytics/run.sh
+++ community_analysis/a_TrajectoryAnalytics/run.sh
@@ -15,8 +15,12 @@
 #    python -c "from a3_log_extraction import get_driver_log; get_driver_log('09$i', 17742)" &
 #done
 
+#for i in 0{1..9} {10..11}; do
+#    python -c "from a3_log_extraction import get_drivers_log; get_drivers_log('09$i', [20318, 15078, 35650, 3413, 13851, 37446, 35685, 33796])" &
+#done
+
 for i in 0{1..9} {10..11}; do
-    python -c "from a3_log_extraction import get_drivers_log; get_drivers_log('09$i', [20318, 15078, 35650, 3413, 13851, 37446, 35685, 33796])" &
+    python -c "from a3_log_extraction import get_drivers_log; get_drivers_log('09$i', [40970, 35716, 30309, 22412, 17695, 17637, 16606, 14969, 12887, 3413])" &
 done
 
 
